f70af44ae710c93a0f8f8e2a022c785575f50dd5
root <rijad.zuzo@gmail.com>
2021-09-04 23:52:14 +0200

    Rename env variables to better match dokku/heroku naming conventions

--- api/infrastructure/infrastructure.sequelize.ts
+++ api/infrastructure/infrastructure.sequelize.ts
@@ -4,17 +4,21 @@ import { User } from '../modules/user/user.model';
 let sequelize: Sequelize;
 
 const initializeSequelize = () => {
-  if (!process.env.DB_HOST || !process.env.DB_NAME || !process.env.DB_USER) {
+  if (
+    !process.env.DATABASE_URL ||
+    !process.env.DATABASE_NAME ||
+    !process.env.DATABASE_USER
+  ) {
     throw new Error('Missing DB configuration in env');
   }
 
   if (!sequelize) {
     sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
+      process.env.DATABASE_NAME,
+      process.env.DATABASE_USER,
+      process.env.DATABASE_PASSWORD,
       {
-        host: process.env.DB_HOST,
+        host: process.env.DATABASE_URL,
         dialect: 'postgres',
         pool: {
           max: 5,
