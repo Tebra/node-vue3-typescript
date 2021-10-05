81a448be644f4dd25dacad94db19a169f58bb98a
root <rijad.zuzo@gmail.com>
2021-10-05 13:34:34 +0200

    Refactor environment configurations

--- api/src/infrastructure/infrastructure.sequelize.ts
+++ api/src/infrastructure/infrastructure.sequelize.ts
@@ -4,21 +4,38 @@ import { User } from '../modules/user/user.model';
 let sequelize: Sequelize;
 
 const initializeSequelize = () => {
+  let isConnectionUrlDefined = false;
+
+  if (process.env.DATABASE_URL) {
+    isConnectionUrlDefined = true;
+  }
   if (
-    !process.env.DATABASE_URL ||
-    !process.env.DATABASE_NAME ||
-    !process.env.DATABASE_USER
+    !isConnectionUrlDefined &&
+    (!process.env.DATABASE_HOST ||
+      !process.env.DATABASE_NAME ||
+      !process.env.DATABASE_USER ||
+      !process.env.DATABASE_PASSWORD)
   ) {
     throw new Error('Missing DB configuration in env');
   }
 
-  if (!sequelize) {
+  if (!sequelize && isConnectionUrlDefined) {
+    sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
+      models: [User],
+    });
+  } else if (!sequelize) {
     sequelize = new Sequelize(
-      process.env.DATABASE_NAME,
-      process.env.DATABASE_USER,
-      process.env.DATABASE_PASSWORD,
+      process.env.DATABASE_NAME as string,
+      process.env.DATABASE_USER as string,
+      process.env.DATABASE_PASSWORD as string,
       {
-        host: process.env.DATABASE_URL,
+        host: process.env.DATABASE_HOST,
         dialect: 'postgres',
         pool: {
           max: 5,
--- ui/src/composables/useAxios.ts
+++ ui/src/composables/useAxios.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosInstance } from 'axios';
 
 export default function useAxios(newBasePath?: string): AxiosInstance {
-  const baseUrl =
-    newBasePath || process.env.VUE_APP_API_HOST || 'localhost:3000';
+  const baseUrl = newBasePath || process.env.BASE_URL || 'localhost';
+  const port = process.env.VUE_APP_API_PORT
+    ? `:${process.env.VUE_APP_API_PORT}`
+    : ':3000';
+
   return axios.create({
-    baseURL: `${baseUrl}/api/`,
+    baseURL: `${baseUrl}${port}/api/`,
     timeout: 1000,
   });
 }
